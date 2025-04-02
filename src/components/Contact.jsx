62312a8e5f3024d0600d73e75433100643fb7308
Shourya Pratap <shouryapratap@Shouryas-MacBook-Pro.local>
2025-04-02 18:42:53 +0530

    Project added

---
+++ src/components/Contact.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { motion } from "framer-motion"
+
+const Contact = () => {
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "6e1331f0-02e3-4015-a9ed-91995856ce37");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("");
+            alert("Form Submitted Successfully")
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            alert(data.message)
+            setResult("");
+        }
+    };
+    return (
+        <motion.div 
+        initial={{ opacity: 0, y: -200 }}
+      transition={{ duration: 1.5 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+        className='container mx-auto py-10 lg:px-32 w-full overflow-hidden' id='Testimonials'>
+            <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>
+                Contact <span className='underline underline-offset-4 decoration-1 under font-light'>With Us</span>
+            </h1>
+            <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>
+                Ready to Make a Move? Let's Built Your Future Together
+            </p>
+            <form onSubmit={onSubmit} className='max-w-2xl mx-auto text-gray-600 pt-8'>
+                <div className='flex flex-wrap'>
+                    <div className='w-full md:w-1/2 text-left'>
+                        Your Name
+                        <input
+                            className='w-full border border-gray-300 rounded py-3 px-4 mt-2'
+                            type="text"
+                            name='Name'
+                            placeholder='Your Name'
+                            required
+                        />
+                    </div>
+                    <div className='w-full md:w-1/2 text-left md:pl-4'>
+                        Your E-mail
+                        <input
+                            className='w-full border border-gray-300 rounded py-3 px-4 mt-2'
+                            type="email"
+                            name='Email'
+                            placeholder='Your Email'
+                            required
+                        />
+                    </div>
+                </div>
+                <div className='my-6 text-left'>
+                    Message
+                    <textarea
+                        className='w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none'
+                        name='Message'
+                        placeholder='Message'
+                        required
+                    ></textarea>
+                </div>
+                <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'>
+                    {result?result:"Send Message"}
+                </button>
+            </form>
+
+        </motion.div>
+
+
+    )
+}
+
+export default Contact
